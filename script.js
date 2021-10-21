917fc6beef278e8cdeee0b0c297893f7937bc0a2
EddieFreedome <eddie.freed@tutanota.com>
2021-10-21 15:46:07 +0200

    creato array oggetti membri team

--- script.js
+++ script.js
@@ -2,6 +2,40 @@
 //membro del team. Ogni membro dovrà avere le informazioni necessarie
 // per stampare la relativa card: Nome, Ruolo e Foto.
 
+//creo array
+const team = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "img/wayne-barnett-founder-ceo.jpg"
+    }
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "img/angela-caroll-chief-editor.jpg"
+    }
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Managaer",
+        foto: "img/walter-gordon-office-manager.jpg"
+    }
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Managaer",
+        foto: "img/angela-caroll-chief-editor.jpg"
+    }
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "img/scott-estrada-developer.jpg"
+    }
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "img/barbara-ramos-graphic-designer.jpg"
+    }
+]
+
 // - Prendendo come riferimento il layout di esempio presente 
 //nell’html, stampiamo tutte le card del nostro team.
 
