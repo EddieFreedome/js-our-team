f11bd5f01d08d7ac5c20047b09e57ea42afc64c4
EddieFreedome <eddie.freed@tutanota.com>
2021-10-21 15:26:37 +0200

    added script with comments

---
+++ script.js
@@ -0,0 +1,16 @@
+// - Creiamo il nostro array di oggetti che rappresentano ciascun 
+//membro del team. Ogni membro dovrà avere le informazioni necessarie
+// per stampare la relativa card: Nome, Ruolo e Foto.
+
+// - Prendendo come riferimento il layout di esempio presente 
+//nell’html, stampiamo tutte le card del nostro team.
+
+// - Utilizziamo poi gli input presenti nella pagina per
+// permettere all’utente di aggiungere nuovi membri del team.
+
+//creo array oggetti per ogni membro del team
+//Nome, ruolo , foto
+
+//Generare card in html del team
+
+//aggiungere input per aggiungere card personalizzate
